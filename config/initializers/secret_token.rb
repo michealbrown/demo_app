b45e2d4de134b7648eaa78a0a69019198c742d26
Micheal Brown <mike@androidmike.com>
2011-11-23 10:47:07 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'fb13ffa1f9b26c8e8900a3320d74cda760f1d3721b31246197421a310c6e21ade6e6606969626989e4f6ec26c8079a863e372be295aa0a6ec48b5de1d84d3543'
